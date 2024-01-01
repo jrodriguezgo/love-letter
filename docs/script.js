50e109808d2193d971ad6da9e79f290f270837c2
jrodriguezgo <jaime.stone@hotmail.com>
2024-01-01 03:17:57 +0100

    subir primera version

--- docs/script.js
+++ docs/script.js
@@ -1,32 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var miAudio = document.getElementById('miAudio');
+      miAudio.currentTime = 20;
+      miAudio.play();
+      openEnvelope()
+
+      setInterval(openEnvelope, 60000);
+});
+
+function openEnvelope() {
     const envelope = document.getElementById('main-envelope');
     const message = document.getElementById('message');
     const leftImage = document.getElementById('left-image');
     const rightImage = document.getElementById('right-image');
     const bottomImage = document.getElementById('bottom-image');
 
-    envelope.addEventListener('click', function () {
-        message.classList.toggle('hidden');
-        message.style.transform = 'translate(-50%, -50%) scale(1)';
+    let now = new Date();
+    const limitTime = new Date();
+    limitTime.setHours(7, 0, 0);
 
-        // Mostramos las imágenes con un efecto de delay
-        setTimeout(function () {
-            leftImage.classList.remove('hidden-image');
-            rightImage.classList.remove('hidden-image');
-            bottomImage.classList.remove('hidden-image');
-            bottomImage.classList.add('visible');
-        }, 500);
-    })
+    if (now >= limitTime){
+        document.getElementById('main-envelope').style.cursor = 'pointer';
 
-    document.addEventListener('click', function (event) {
-        if (!envelope.contains(event.target) && !message.contains(event.target)) {
-            message.classList.add('hidden');
-            message.style.transform = 'translate(-50%, -50%) scale(0)';
+        envelope.addEventListener('click', function () {
+            message.classList.toggle('hidden');
+            message.style.transform = 'translate(-50%, -50%) scale(1)';
+    
+            // Mostramos las imágenes con un efecto de delay
+            setTimeout(function () {
+                leftImage.classList.remove('hidden-image');
+                rightImage.classList.remove('hidden-image');
+                bottomImage.classList.remove('hidden-image');
+                bottomImage.classList.add('visible');
+            }, 500);
+        })
 
-            // Quitamos a los finics
-            leftImage.classList.add('hidden-image');
-            rightImage.classList.add('hidden-image');
-            bottomImage.classList.add('hidden-image')
-        }
-    });
-});
+        document.addEventListener('click', function (event) {
+            if (!envelope.contains(event.target) && !message.contains(event.target)) {
+                message.classList.add('hidden');
+                message.style.transform = 'translate(-50%, -50%) scale(0)';
+    
+                // Quitamos a los finics
+                leftImage.classList.add('hidden-image');
+                rightImage.classList.add('hidden-image');
+                bottomImage.classList.add('hidden-image')
+            }
+        });
+    }
+}
